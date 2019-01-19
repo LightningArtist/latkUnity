add1ef3997cfe660f8ba692259156d9b7559a9cd
Nick Fox-Gieg <nick@fox-gieg.com>
2019-01-18 21:07:41 -0500

    add quick demo script

---
+++ Scripts/Playback/LatkQuickDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatkQuickDemo : MonoBehaviour {
+
+    public LightningArtist latk;
+    public int startingFrames = 12;
+
+    private bool firstRun = true;
+
+    private void Awake() {
+        if (latk == null) latk = GetComponent<LightningArtist>();
+    }
+
+    private void Update() {
+        if (firstRun) {
+            for (int i = 0; i < startingFrames - 1; i++) {
+                latk.inputNewFrame();
+            }
+            latk.inputPlay();
+            firstRun = false;
+        }
+    }
+
+}
